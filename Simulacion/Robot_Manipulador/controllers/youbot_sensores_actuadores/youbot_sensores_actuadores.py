46e8296d7eae83a9ae52ab3e54c4f1c2170e927d
fer <fernandorecalde@uti.edu.ec>
2024-01-25 11:33:05 -0500

    init system

--- Simulacion/Robot_Manipulador/controllers/youbot_sensores_actuadores/youbot_sensores_actuadores.py
+++ Simulacion/Robot_Manipulador/controllers/youbot_sensores_actuadores/youbot_sensores_actuadores.py
@@ -35,23 +35,30 @@ def main(robot):
     # Definition of the desired angles for each joint in the manipulator
     q_c = np.zeros((5, t.shape[0]), dtype = np.double)
 
+    # Definition of the desired angular velocities for each joint in the manipulator
+    qp_c = np.zeros((5, t.shape[0]), dtype = np.double)
+
     # Definition of the desired angles for each joint in the manipulator
     q = np.zeros((5, t.shape[0] + 1), dtype = np.double)
 
-    # get initial conditions of the system
-    q[:, 0] = get_angular_position(robot, sensors, time_step)
     
 
+    # Init System
+    init_system(robot, motors, [0.0, 0.0, 0.0, 0.0, 0.0], time_step, t_s)
+    # get initial conditions of the system
+    q[:, 0] = get_angular_position(robot, sensors, time_step)
 
+    # Simulation loop
     for k in range(0, t.shape[0]):
          if robot.step(time_step) != -1:
             tic = time.time()
             print("Angular displacement")
             print(q[:, k])
             # Compute desired values
+            qp_c[:, k] = [0.1, 0.0, 0.0, 0.0, 0.0]
 
             # actuate the rotational motors of the system
-            set_motor(motors, q_c[:, k])
+            set_motor_vel(motors, qp_c[:, k])
 
             # Get current states of the system
             q[:, k + 1] = get_angular_position(robot, sensors, time_step)
@@ -75,8 +82,8 @@ def get_motor(robot, name):
         motor.append(robot.getDevice(k))
     return motor
 
-def set_motor(motor, pos):
-    # Set motor with velocity control
+def set_motor_pos(motor, pos):
+    # Set motor with position control
     # INPUT
     # motor                                             - device motor
     # pos                                               - desired position for each motor
@@ -85,6 +92,19 @@ def set_motor(motor, pos):
     size = len(motor)
     for k in range(0, size):
         motor[k].setPosition(pos[k])
+    return None
+
+def set_motor_vel(motor, vel):
+    # Set motor with velocity control
+    # INPUT
+    # motor                                             - device motor
+    # vel                                               - desired velocity for each motor
+    # OUTPUT
+    # None
+    size = len(motor)
+    for k in range(0, size):
+        motor[k].setPosition(float('inf'))
+        motor[k].setVelocity(vel[k])
     return None
 
 def get_sensor_pos(names, time_step):
@@ -115,6 +135,27 @@ def get_angular_position(robot, sensors, time_step):
             data = sensors[k].getValue()
             q[k] = data
     return q
+def init_system(robot, motors, q_c, time_step, t_s):
+    # Function that moves the robot to an initial configuration
+    # INPUT 
+    # robot                                        - A robot object that contains all the required information
+    # motors                                       - A list with the required motor objects
+    # q_c                                          - A vector of desired initial angular values
+    # time_step                                    - sample time of the simulation webots
+    # ts                                           - sample time of the simulation
+    # OUTPUT
+    # None
+    for k in range(0, 50):
+         if robot.step(time_step) != -1:
+            tic = time.time()
+            print("Init System")
+            set_motor_pos(motors, q_c)
+            # Sample time saturation
+            while (time.time() - tic <= t_s):
+                None
+            toc = time.time() - tic 
+    return None
+
 
 if __name__ == '__main__':
     try:
